88235f4c352711b22bd85460181a7f1450f44ada
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-07-31 20:04:58 +0200

    Returning nearby stops

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
+const fs = require('fs');
 const express = require('express');
+const request = require('request-promise-native');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hej!'));
+const key = ('' + fs.readFileSync('key.txt')).trim();
+
+console.log(`Using API key ${key}`);
+
+app.get('/', async (req, res) => {
+
+  const options = {
+    uri: 'https://api.resrobot.se/v2/location.nearbystops',
+    qs: {
+      key: key,
+      originCoordLat: 56.4883233,
+      originCoordLong: 16.3842807,
+      r: 10000,
+      format: 'json'
+    },
+    json: true
+  };
+
+  const response = await request(options);
+
+  res.json(response);
+});
 
 app.listen(port, () => console.log(`Take-me-home app listening on port ${port}!`));
