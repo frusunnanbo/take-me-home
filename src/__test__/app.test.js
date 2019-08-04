5dbe3c4a828c95713102b352cabd88c9d469aba0
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-08-04 19:27:22 +0200

    Offline, stable fixture test in place

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,9 +1,37 @@
 const request = require('supertest');
+const nock = require('nock');
+const fs = require('fs');
 const app = require('../app');
 
 describe('The Take-me-home app', () => {
 
   jest.setTimeout(60000);
+
+  beforeAll(() => {
+    nock('https://api.resrobot.se')
+      .get(/\/v2\/trip.*date=2019-08-04.*time=19.*/)
+      .reply(200, JSON.parse(fs.readFileSync('response1.json')));
+
+    nock('https://api.resrobot.se')
+      .get(/\/v2\/trip.*time=05.*/)
+      .reply(200, JSON.parse(fs.readFileSync('response2.json')));
+
+    nock('https://api.resrobot.se')
+      .get(/\/v2\/trip.*time=07.*/)
+      .reply(200, JSON.parse(fs.readFileSync('response3.json')));
+
+    nock('https://api.resrobot.se')
+      .get(/\/v2\/trip.*time=10.*/)
+      .reply(200, JSON.parse(fs.readFileSync('response4.json')));
+
+    nock('https://api.resrobot.se')
+      .get(/\/v2\/trip.*time=14.*/)
+      .reply(200, JSON.parse(fs.readFileSync('response5.json')));
+
+    nock('https://api.resrobot.se')
+      .get(/\/v2\/trip.*date=2019-08-05.*time=19.*/)
+      .reply(200, JSON.parse(fs.readFileSync('response6.json')));
+  });
 
   it('Returns some trips', (done) => {
 
