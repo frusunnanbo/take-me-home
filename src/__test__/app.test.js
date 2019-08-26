fa1c481b143445403494fea5fba8f6c4fbabccd1
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-08-26 21:06:15 +0200

    Working fixpoint test

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -16,7 +16,7 @@ describe('The Take-me-home app', () => {
   it('Returns some trips', (done) => {
 
     request(app)
-      .get('/?hereLat=56.4883233&hereLong=16.3842807&homeLat=59.2733699&homeLong=18.0183112&startTime=2019-08-04T19:02')
+      .get('/trips?startTime=2019-08-04T19:02')
       .expect(200)
       .end((err, res) => {
         expect(res.body).toMatchSnapshot();
