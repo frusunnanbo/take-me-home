26a3fc750897ae1223038cbff72c3115d18a0840
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-08-27 10:22:27 +0200

    Expect 200

--- src/__tests__/fixture.test.js
+++ src/__tests__/fixture.test.js
@@ -1,6 +1,13 @@
+const request = require('supertest');
+const app = require('../app');
+
 describe('The Take Me Home App', () => {
 
-  it('can return some trips on request', () => {
-    expect(true).toBeTrue();
+  jest.setTimeout(60000);
+
+  it('can return some trips on request', (done) => {
+    request(app)
+      .get('/trips')
+      .expect(200, done);
   });
 });
