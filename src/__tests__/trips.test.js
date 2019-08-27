e997832f36e8beea4a41e5773af77e1203992f6b
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-08-27 15:03:39 +0200

    Cleaner selection test.

--- src/__tests__/trips.test.js
+++ src/__tests__/trips.test.js
@@ -11,8 +11,7 @@ describe('The trip-selecting function', () => {
   it('returns a single element unchanged', () => {
     const trip1 = trip({
       start: '08:30',
-      duration: moment.duration(1, 'hour'),
-      numberOfLegs: 1
+      duration: moment.duration(1, 'hour')
     });
 
     expect(selectAndSort([trip1])).toEqual([trip1]);
@@ -21,13 +20,11 @@ describe('The trip-selecting function', () => {
   it('returns two trips with different starttimes unchanged', () => {
     const trip1 = trip({
       start: '08:30',
-      duration: moment.duration(1, 'hour'),
-      numberOfLegs: 1
+      duration: moment.duration(1, 'hour')
     });
     const trip2 = trip({
       start: '09:30',
-      duration: moment.duration(1, 'hour'),
-      numberOfLegs: 1
+      duration: moment.duration(1, 'hour')
     });
 
     expect(selectAndSort([trip1, trip2])).toEqual([trip1, trip2]);
@@ -36,43 +33,11 @@ describe('The trip-selecting function', () => {
   it('selects the shortest trip of two with equal starttimes', () => {
     const trip1 = trip({
       start: '08:30',
-      duration: moment.duration(2, 'hours'),
-      numberOfLegs: 1
+      duration: moment.duration(2, 'hours')
     });
     const trip2 = trip({
       start: '08:30',
-      duration: moment.duration(1, 'hour'),
-      numberOfLegs: 1
-    });
-
-    expect(selectAndSort([trip1, trip2])).toEqual([trip2]);
-  });
-
-  it('sorts durations correctly with respect to minutes', () => {
-    const trip1 = trip({
-      start: '08:30',
-      duration: moment.duration({hours: 1, minutes: 22}),
-      numberOfLegs: 1
-    });
-    const trip2 = trip({
-      start: '08:30',
-      duration: moment.duration({hours: 1, minutes: 23}),
-      numberOfLegs: 1
-    });
-
-    expect(selectAndSort([trip1, trip2])).toEqual([trip1]);
-  });
-
- it('sorts durations correctly with respect to hours', () => {
-    const trip1 = trip({
-      start: '08:30',
-      duration: moment.duration({hours: 2, minutes: 22}),
-      numberOfLegs: 1
-    });
-    const trip2 = trip({
-      start: '08:30',
-      duration: moment.duration({hours: 1, minutes: 23}),
-      numberOfLegs: 1
+      duration: moment.duration(1, 'hour')
     });
 
     expect(selectAndSort([trip1, trip2])).toEqual([trip2]);
@@ -81,20 +46,16 @@ describe('The trip-selecting function', () => {
   it('does not group a third trip with a different starttime', () => {
     const trip1 = trip({
       start: '08:30',
-      duration: moment.duration(2, 'hours'),
-      numberOfLegs: 1
+      duration: moment.duration(2, 'hours')
     });
     const trip2 = trip({
       start: '08:30',
-      duration: moment.duration(1, 'hour'),
-      numberOfLegs: 1
+      duration: moment.duration(1, 'hour')
     });
     const trip3 = trip({
       start: '09:30',
-      duration: moment.duration(2, 'hours'),
-      numberOfLegs: 1
+      duration: moment.duration(2, 'hours')
     });
-
 
     expect(selectAndSort([trip1, trip2, trip3])).toEqual([trip2, trip3]);
   });
@@ -111,84 +72,6 @@ function trip(properties) {
       "hours": properties.duration ? properties.duration.get('hours') : 1,
       "minutes": properties.duration ? properties.duration.get('minutes') : 2
     },
-    "legs": [
-      {
-        "start": {
-          "date": "2019-08-28",
-          "time": "12:49",
-          "place": "Kungsporten (Jönköping kn)"
-        },
-        "end": "2019-08-28T13:10:00",
-        "stops": [
-          "Kungsporten (Jönköping kn)",
-          "Kittendorffs gata (Jönköping kn)",
-          "Kungsängen (Jönköping kn)",
-          "Gullregnsbacken (Jönköping kn)",
-          "Resedastigen (Jönköping kn)",
-          "Jönköping Ekhagen centrum",
-          "Rosenlundsskolan (Jönköping kn)",
-          "Hermansplan (Jönköping kn)",
-          "Rosenlunds Vårdcentrum (Jönköping kn)",
-          "Starrgatan (Jönköping kn)",
-          "Jönköping Kilallén",
-          "Jönköping Cigarren",
-          "Östra torget (Jönköping kn)",
-          "Stadsbiblioteket (Jönköping kn)",
-          "Jönköping Östra centrum",
-          "Jönköping Rådhusparken",
-          "Jönköping Juneporten"
-        ],
-        "type": "JNY"
-      },
-      {
-        "start": {
-          "date": "2019-08-28",
-          "time": "13:20",
-          "place": "Jönköping Centralstation"
-        },
-        "end": "2019-08-28T18:30:00",
-        "stops": [
-          "Jönköping Centralstation",
-          "Linköping Fjärrbussterminal",
-          "Norrköping busstation",
-          "Stockholm Cityterminalen"
-        ],
-        "type": "JNY"
-      },
-      {
-        "start": {
-          "date": "2019-08-28",
-          "time": "18:51",
-          "place": "T-Centralen T-bana (Stockholm kn)"
-        },
-        "end": "2019-08-28T19:00:00",
-        "stops": [
-          "T-Centralen T-bana (Stockholm kn)",
-          "Gamla Stan T-bana (Stockholm kn)",
-          "Slussen T-bana (Stockholm kn)",
-          "Medborgarplatsen T-bana (Stockholm kn)",
-          "Skanstull T-bana (Stockholm kn)",
-          "Gullmarsplan T-bana (Stockholm kn)"
-        ],
-        "type": "JNY"
-      },
-      {
-        "start": {
-          "date": "2019-08-28",
-          "time": "19:08",
-          "place": "Gullmarsplan T-bana (Stockholm kn)"
-        },
-        "end": "2019-08-28T19:11:00",
-        "stops": [
-          "Gullmarsplan T-bana (Stockholm kn)",
-          "Sköntorpsplan (Stockholm kn)",
-          "Ymsenvägen (Stockholm kn)",
-          "Skagersvägen (Stockholm kn)",
-          "Vättersvägen (Stockholm kn)",
-          "Tämnarvägen (Stockholm kn)"
-        ],
-        "type": "JNY"
-      }
-    ]
+    "legs": properties.legs || []
   };
 }
