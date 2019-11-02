df949f67ea4861abd5abce10e94108168b897f70
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-11-02 21:23:47 +0100

    Snapshot! A snapshot

--- src/__tests__/fixture.test.js
+++ src/__tests__/fixture.test.js
@@ -5,7 +5,10 @@ const app = require('../app');
 describe('The take-me-home app', () => {
   it('Responds to /', () => {
     return request(app)
-      .get('/trips')
-      .expect(200);
+      .get('/trips?startTime=2019-11-09T11:31')
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toMatchSnapshot();
+      });
   })
 });
