d74cc9f941b0806ed7492d8b90c5998d71b6e842
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-11-02 22:02:01 +0100

    Testing the trips module

---
+++ src/__tests__/trips.test.js
@@ -0,0 +1,59 @@
+const moment = require('moment');
+
+const { selectTrips } = require('../trips');
+
+describe('The trip-selecting function', () => {
+
+  it('returns an empty list unchanged', () => {
+    expect(selectTrips([])).toBeEmpty();
+  });
+
+  it('returns a single element unchanged', () => {
+    const trip1 = trip({
+      start: '08:30',
+      duration: moment.duration(1, 'hour')
+    });
+
+    expect(selectTrips([trip1])).toEqual([trip1]);
+  });
+
+  it('returns two trips with different starttimes unchanged', () => {
+    const trip1 = trip({
+      start: '08:30',
+      duration: moment.duration(1, 'hour')
+    });
+    const trip2 = trip({
+      start: '09:30',
+      duration: moment.duration(1, 'hour')
+    });
+
+    expect(selectTrips([trip1, trip2])).toEqual([trip1, trip2]);
+  });
+
+  it('selects the shortest trip of two with equal starttimes', () => {
+    const trip1 = trip({
+      start: '08:30',
+      duration: moment.duration(2, 'hours')
+    });
+    const trip2 = trip({
+      start: '08:30',
+      duration: moment.duration(1, 'hour')
+    });
+
+    expect(selectTrips([trip1, trip2])).toEqual([trip2]);
+  });
+});
+
+function trip(properties) {
+  return {
+    "start": {
+      "date": "2019-08-28",
+      "time": properties.start || '06:00'
+    },
+    "duration": {
+      "hours": properties.duration ? properties.duration.get('hours') : 1,
+      "minutes": properties.duration ? properties.duration.get('minutes') : 2
+    },
+    "legs": properties.legs || []
+  };
+}
