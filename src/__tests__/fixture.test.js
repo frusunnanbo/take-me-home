b3ad805b74ca9dff3d27bd2320c25e7e13af57b6
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-11-05 15:57:13 +0100

    Adds fixed test starttime, updates snapshot

--- src/__tests__/fixture.test.js
+++ src/__tests__/fixture.test.js
@@ -5,7 +5,7 @@ describe('The Take-me-home app', () => {
 
   it('returns some trips', () => {
     return request(app)
-      .get('/trips')
+      .get('/trips?startTime=2019-11-09T12:15')
       .expect(200)
       .then((response) => {
         expect(response.body).toMatchSnapshot();
