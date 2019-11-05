ca7c9312f3d51d912bf39198d7a02b6c536051ca
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-11-05 16:06:25 +0100

    Fixed response from resrobot

--- src/__tests__/fixture.test.js
+++ src/__tests__/fixture.test.js
@@ -1,7 +1,16 @@
 const request = require('supertest');
+const nock = require('nock');
+
 const app = require('../app');
 
+
 describe('The Take-me-home app', () => {
+
+  beforeEach(() => {
+    nock('https://api.resrobot.se')
+      .get(/.*/)
+      .replyWithFile(200, 'resrobot.response.json');
+  });
 
   it('returns some trips', () => {
     return request(app)
