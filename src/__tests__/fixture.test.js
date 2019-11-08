8f16999dddf7bfcf07bc58abc7d64cccfa20837b
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-11-08 16:36:06 +0100

    Fake response with nock

--- src/__tests__/fixture.test.js
+++ src/__tests__/fixture.test.js
@@ -1,8 +1,15 @@
 const request = require('supertest');
+const nock = require('nock');
 
 const app = require('../app');
 
 describe('The Take-me-home app', () => {
+
+  beforeEach(() => {
+    nock('https://api.resrobot.se')
+      .get(/.*/)
+      .replyWithFile(200, 'resrobot.response.json');
+  })
 
   it('returns some trips', () => {
 
