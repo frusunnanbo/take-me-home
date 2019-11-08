59a042cccc0e7b8b9157333b33e5bb5cd2ff35c2
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-11-08 16:25:40 +0100

    A snapshot test

--- src/__tests__/fixture.test.js
+++ src/__tests__/fixture.test.js
@@ -1,6 +1,16 @@
+const request = require('supertest');
+
+const app = require('../app');
+
 describe('The Take-me-home app', () => {
 
   it('returns some trips', () => {
-    expect(true).toBeTrue();
-  })
+
+    return request(app)
+      .get('/trips')
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toMatchSnapshot();
+      })
+  }, 60000)
 })
